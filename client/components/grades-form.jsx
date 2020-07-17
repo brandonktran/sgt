92c35a579c5a0621a8fa6b279c1a0f32bcf92b8c
brandonktran <brandontran@berkeley.edu>
2020-07-17 17:13:16 +0000

    fixed indentation and name property on event

--- client/components/grades-form.jsx
+++ client/components/grades-form.jsx
@@ -1,83 +1,128 @@
-/* eslint-disable indent */
 import React from 'react';
 
 export default class GradeForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            course: '',
-            grade: ''
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleReset = this.handleReset.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      course: '',
+      grade: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
 
-    handleChange(event) {
-        const property = event.target.placeholder.toLowerCase();
-        this.setState({
-            [property]: event.target.value
-        });
-    }
+  handleChange(event) {
+    const property = event.target.placeholder.toLowerCase();
+    this.setState({
+      [property]: event.target.value
+    });
+  }
 
-    handleSubmit(event) {
-        event.preventDefault();
-        const newGrade = {
-            name: this.state.name,
-            course: this.state.course,
-            grade: this.state.grade
-        };
-        this.props.onSubmit(newGrade);
-        this.setState({
-            name: '',
-            course: '',
-            grade: ''
-        });
-    }
+  handleSubmit(event) {
+    event.preventDefault();
+    const newGrade = {
+      name: this.state.name,
+      course: this.state.course,
+      grade: this.state.grade
+    };
+    this.props.onSubmit(newGrade);
+    this.setState({
+      name: '',
+      course: '',
+      grade: ''
+    });
+  }
 
-    handleReset() {
-        this.setState({
-            name: '',
-            course: '',
-            grade: ''
-        });
-    }
+  handleReset() {
+    this.setState({
+      name: '',
+      course: '',
+      grade: ''
+    });
+  }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} onReset={this.handleReset} className="mt-4">
-                <div className="form-group">
-                    <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                    </svg>
-                    <input required autoFocus type="text" placeholder="Name"
-                        value={this.state.name}
-                        onChange={this.handleChange} />
-                </div>
-                <div className="form-group">
-                    <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-journal-text" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2h1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1H2a2 2 0 0 1 2-2z" />
-                        <path d="M2 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2z" />
-                        <path fillRule="evenodd" d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
-                    </svg>
-                    <input required autoFocus type="text" placeholder="Course"
-                        value={this.state.course}
-                        onChange={this.handleChange} />
-                </div>
-                <div className="form-group">
-                    <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-calculator" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm2 .5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-2zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zM4.5 9a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM4 12.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zM7.5 6a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM7 9.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm.5 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM10 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm.5 2.5a.5.5 0 0 0-.5.5v4a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 0-.5-.5h-1z" />
-                    </svg>
-                    <input required autoFocus type="number" placeholder="Grade"
-                        value={this.state.grade}
-                        onChange={this.handleChange} />
-                </div>
-                <div>
-                    <button type="submit" className="btn btn-primary m-1">Add</button>
-                    <button type="reset" className="btn btn-danger m-1">Cancel</button>
-                </div>
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} onReset={this.handleReset} className="mt-4">
+        <div className="form-group">
+          <svg
+            width="2em"
+            height="2em"
+            viewBox="0 0 16 16"
+            className="bi bi-person-fill"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+            />
+          </svg>
+          <input
+            required
+            autoFocus
+            type="text"
+            name="Name"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <svg
+            width="2em"
+            height="2em"
+            viewBox="0 0 16 16"
+            className="bi bi-journal-text"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2h1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1H2a2 2 0 0 1 2-2z" />
+            <path d="M2 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2z" />
+            <path
+              fillRule="evenodd"
+              d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+          <input
+            required
+            type="text"
+            name="Course"
+            placeholder="Course"
+            value={this.state.course}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <svg
+            width="2em"
+            height="2em"
+            viewBox="0 0 16 16"
+            className="bi bi-calculator"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm2 .5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-2zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zM4.5 9a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM4 12.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zM7.5 6a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM7 9.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm.5 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM10 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm.5 2.5a.5.5 0 0 0-.5.5v4a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 0-.5-.5h-1z"
+            />
+          </svg>
+          <input
+            required
+            type="number"
+            name="Grade"
+            placeholder="Grade"
+            value={this.state.grade}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit" className="btn btn-primary m-1">Add</button>
+          <button type="reset" className="btn btn-danger m-1">Cancel</button>
+        </div>
+      </form>
+    );
+  }
 }
