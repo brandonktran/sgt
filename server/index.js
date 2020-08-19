00d47052ba61bcf56bb3ddad896aa98e11f40041
brandonktran <brandontran@berkeley.edu>
2020-08-19 18:28:54 +0000

    changed port

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const endpoints = jsonServer.router(dbPath);
 
 server.use(middleware);
 server.use('/api', endpoints);
-server.listen(3001, () => {
+server.listen(3003, () => {
   // eslint-disable-next-line no-console
   console.log('JSON Server listening on port 3001\n');
 });
